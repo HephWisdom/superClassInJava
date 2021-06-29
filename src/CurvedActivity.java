dbd466c02d0666c04bed71b61b68fb35a284aac9
Williams <williamswisdom108@gmail.com>
2021-06-29 16:45:52 +0000

    initial commit

---
+++ src/CurvedActivity.java
@@ -0,0 +1,30 @@
+
+public class CurvedActivity extends GradeActivity {
+	
+	private double rawscore;
+	private double percentage;
+	
+	public CurvedActivity(double percent) {
+		this.percentage = percent;
+
+	}
+
+	
+	//a method for setScore
+	public void setScore(double setScore) {
+	
+	}
+	
+	//a method for getPercentage
+	public char getPercentage() {
+		return 0;
+	}
+	
+	//a method for getRawScore
+	public double getRawScore() {
+		return 0;
+	}
+	
+	
+		
+}
---
+++ src/GradeActivity.java
@@ -0,0 +1,23 @@
+
+public class GradeActivity {
+	
+	private double score;
+	
+	//a method for setScore
+	public void setScore(double setScore) {
+
+	}
+	
+	
+	//a method for getScore
+	public double getScore() {
+		return score;
+	}
+	
+	
+	//a method for getGrade
+	public char getGrade() {
+		return ' ';
+	}
+		
+}
